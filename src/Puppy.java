f9d23eccdae595ee801a8f8aa52c0bb1341fd40c
eyezhan <zhan_lingling@sina.cn>
2018-05-31 23:12:29 +0800

    Add Dog.java and Puppy.java

---
+++ src/Puppy.java
@@ -0,0 +1,9 @@
+public class Puppy{
+    public Puppy(String name){
+        System.out.println("Name of the dog: " + name);
+    }
+
+    public static void main(String []args){
+        Puppy myPuppy = new Puppy("tommy");
+    }
+}
