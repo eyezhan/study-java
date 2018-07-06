348e9d4bb432c8d65da5c1a291beb1660507b174
Zhan Lingling <zhanlingling_00@163.com>
2018-07-06 18:59:13 +0800

    Update Puppy.java

--- src/Puppy.java
+++ src/Puppy.java
@@ -1,9 +1,25 @@
 public class Puppy{
+    int puppyAge;
     public Puppy(String name){
         System.out.println("Name of the dog: " + name);
     }
 
+    public void setAge( int age ){
+        puppyAge = age;
+    }
+
+    public int getAge(){
+        System.out.println("Age of dog: " + puppyAge);
+        return puppyAge;
+    }
+
     public static void main(String []args){
+        /* Create object */
         Puppy myPuppy = new Puppy("tommy");
+        /* Set age */
+        myPuppy.setAge(2);
+        /* Get age */
+        myPuppy.getAge();
+        System.out.println("Value of puppyAge: " + myPuppy.puppyAge);
     }
 }
