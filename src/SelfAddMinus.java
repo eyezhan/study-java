2987c5d3ffe234b5b4436afea18505673800f662
Zhan Lingling <zhanlingling_00@163.com>
2018-07-09 17:20:54 +0800

    Update example about arithmetic operation.

--- src/SelfAddMinus.java
+++ src/SelfAddMinus.java
@@ -6,5 +6,12 @@ public class SelfAddMinus {
         int d = --c;
         System.out.println("进行自增运算后的值等于" + b);
         System.out.println("进行自减运算后的值等于" + d);
+
+        int m = 5;
+        int n = 5;
+        int x = 2 * ++m;
+        int y = 2 * n++;
+        System.out.println("自增运算符前缀运算后m=" + m +", x=" + x);
+        System.out.println("自增运算符后缀运算后n=" + n +", y=" + y);
     }
 }
