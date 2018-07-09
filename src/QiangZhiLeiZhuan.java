882641d3696546bf97f7079eae724d9e96605c16
Zhan Lingling <zhanlingling_00@163.com>
2018-07-09 13:41:03 +0800

    Add type conversion.

---
+++ src/QiangZhiLeiZhuan.java
@@ -0,0 +1,8 @@
+public class QiangZhiLeiZhuan{
+    public static void main(String[] args){
+        int i1 = 123;
+        byte b = (byte)i1;
+        System.out.println("int强制类型转换为byte后的值等于" + b);
+    }
+}
+
