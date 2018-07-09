ff76fd3a2bc5cb76f11e082310620f9b2eab08c5
Zhan Lingling <zhanlingling_00@163.com>
2018-07-09 18:52:58 +0800

    Example about while loop.

---
+++ src/Loop.java
@@ -0,0 +1,9 @@
+public class Loop {
+    public static void main(String[] args) {
+        int x = 10;
+        while(x < 20) {
+            System.out.println("value of x: " + x);
+            x++;
+        }
+    }
+}
