68ea894d609a464934afe62ea9d636bac581101b
Zhan Lingling <zhanlingling_00@163.com>
2018-07-09 13:44:57 +0800

    Add example about class/instance/local variable.

---
+++ src/Variable.java
@@ -0,0 +1,12 @@
+public class Variable{
+    static int allClicks = 0; // class variable
+
+    String str = "hello world"; // instance variable
+
+    public void method(){
+
+        int i = 0; // local variable
+
+    }
+}
+
