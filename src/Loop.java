72dac3596f29ff2ea19d01cdcd944c0fa40a1a1f
Zhan Lingling <zhanlingling_00@163.com>
2018-07-09 18:56:13 +0800

    Example about do...while loop.

--- src/Loop.java
+++ src/Loop.java
@@ -1,9 +1,17 @@
 public class Loop {
     public static void main(String[] args) {
         int x = 10;
+        System.out.println("while");
         while(x < 20) {
             System.out.println("value of x: " + x);
             x++;
         }
+
+        System.out.println("\ndo...while");
+        x = 10;
+        do {
+            System.out.println("value of x: " + x);
+            x++;
+        } while(x < 20);
     }
 }
