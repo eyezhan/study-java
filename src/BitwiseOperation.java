bf8a056435539e7d7f50cc4cf22ae3d68699fd6c
Zhan Lingling <zhanlingling_00@163.com>
2018-07-09 17:44:08 +0800

    Example about bitwise operation.

---
+++ src/BitwiseOperation.java
@@ -0,0 +1,27 @@
+public class BitwiseOperation {
+    public static void main(String[] args) {
+        int a = 60; /* 60 = 0011 1100 */
+        int b = 13; /* 13 = 0000 1101 */
+        int c = 0;
+        c = a & b;
+        System.out.println("a & b = " + c);
+
+        c = a | b;
+        System.out.println("a | b = " + c);
+
+        c = a ^ b;
+        System.out.println("a ^ b = " + c);
+
+        c = ~a;
+        System.out.println("~a = " + c);
+
+        c = a << 2;
+        System.out.println("a << 2 = " + c);
+
+        c = a >> 2;
+        System.out.println("a >> 2 = " + c);
+
+        c = a >>> 2;
+        System.out.println("a >>> 2 = " +c);
+    }
+}
