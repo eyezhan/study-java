8cfbd25fda08962cd45dccc59a9afdf3078bab91
Zhan Lingling <zhanlingling_00@163.com>
2018-07-10 09:38:35 +0800

    Example about branch structure "if".

---
+++ src/BranchStructure.java
@@ -0,0 +1,9 @@
+public class BranchStructure {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x < 20) {
+            System.out.print("这是if语句");
+        }
+    }
+}
