a23d2b7f8e5605348916c53bb739c3ebbd254b02
Zhan Lingling <zhanlingling_00@163.com>
2018-07-10 09:40:52 +0800

    Branch structure: if...else

--- src/BranchStructure.java
+++ src/BranchStructure.java
@@ -1,9 +1,11 @@
 public class BranchStructure {
     public static void main(String[] args) {
-        int x = 10;
+        int x = 30;
 
         if (x < 20) {
             System.out.print("这是if语句");
+        } else {
+            System.out.print("这是else语句");
         }
     }
 }
