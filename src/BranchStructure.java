f62472a936bd1556349942971fa8edd48ebb02c5
Zhan Lingling <zhanlingling_00@163.com>
2018-07-10 10:08:50 +0800

    Branch structure: nested if...else

--- src/BranchStructure.java
+++ src/BranchStructure.java
@@ -3,13 +3,20 @@ public class BranchStructure {
         int x = 30;
 
         if (x == 10) {
-            System.out.print("Value of x is 10");
+            System.out.println("Value of x is 10");
         } else if (x == 20) {
-            System.out.print("Value of x is 20");
+            System.out.println("Value of x is 20");
         } else if (x == 30) {
-            System.out.print("Value of x is 30");
+            System.out.println("Value of x is 30");
         } else {
-            System.out.print("这是else语句");
+            System.out.println("这是else语句");
+        }
+
+        int y = 10;
+        if (x == 30) {
+            if (y == 10) {
+                System.out.println("X = 30 and Y = 10");
+            }
         }
     }
 }
