ce373ea2be78cef5fe7cb673827fd9dfa3ee5044
Zhan Lingling <zhanlingling_00@163.com>
2018-07-10 10:04:51 +0800

    Branch structure: if...else if...else

--- src/BranchStructure.java
+++ src/BranchStructure.java
@@ -2,8 +2,12 @@ public class BranchStructure {
     public static void main(String[] args) {
         int x = 30;
 
-        if (x < 20) {
-            System.out.print("这是if语句");
+        if (x == 10) {
+            System.out.print("Value of x is 10");
+        } else if (x == 20) {
+            System.out.print("Value of x is 20");
+        } else if (x == 30) {
+            System.out.print("Value of x is 30");
         } else {
             System.out.print("这是else语句");
         }
