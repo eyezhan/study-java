f6645f6840b5f0fa76a8f9b40821bf2428515fde
Zhan Lingling <zhanlingling_00@163.com>
2018-07-10 10:14:19 +0800

    Branch structure: switch

--- src/BranchStructure.java
+++ src/BranchStructure.java
@@ -18,5 +18,25 @@ public class BranchStructure {
                 System.out.println("X = 30 and Y = 10");
             }
         }
+
+        // switch
+        char grade = 'C';
+
+        switch(grade) {
+            case 'A':
+                System.out.println("优秀");
+            case 'B':
+            case 'C':
+                System.out.println("良好");
+                break;
+            case 'D':
+                System.out.println("及格");
+            case 'F':
+                System.out.println("你需要再努力努力");
+                break;
+            default:
+                System.out.println("未知等级");
+        }
+        System.out.println("你的等级是 " + grade);
     }
 }
