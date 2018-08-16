629efc6758404e627eeb0f129cb82a19ff05c155
Zhan Lingling <zhanlingling_00@163.com>
2018-08-16 15:05:17 +0800

    Example about finalize function.

---
+++ src/FinalizationDemo.java
@@ -0,0 +1,30 @@
+/*************************************************************************
+	File Name: FinalizationDemo.java
+	Author: Zhan Lingling
+	Mail: zhan_lingling@sina.cn
+	Created Time: Thu Aug 16 14:40:11 2018
+ ************************************************************************/
+
+public class FinalizationDemo {
+    public static void main(String[] args) {
+        Cake c1 = new Cake(1);
+        Cake c2 = new Cake(2);
+        Cake c3 = new Cake(3);
+
+        c2 = c3 = null;
+        System.gc();
+    }
+}
+
+class Cake extends Object {
+    private int id;
+    public Cake(int id) {
+        this.id = id;
+        System.out.println("Cake Object " + id + " is Created");
+    }
+
+    protected void finalize() throws java.lang.Throwable {
+        super.finalize();
+        System.out.println("Cake Object " + id + " is disposed");
+    }
+}
