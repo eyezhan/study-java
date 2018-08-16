0553514d2a5aa2d7351c26efa5433a41b7e5bdbd
Zhan Lingling <zhanlingling_00@163.com>
2018-08-16 14:27:49 +0800

    Construction function with argument.

--- src/ConsDemo.java
+++ src/ConsDemo.java
@@ -1,14 +1,14 @@
 /*************************************************************************
 	File Name: ConsDemo.java
-	Author: 
-	Mail: 
+	Author: Zhan Lingling
+	Mail: zhan_lingling@sina.cn
 	Created Time: Thu Aug 16 14:14:18 2018
  ************************************************************************/
 
 public class ConsDemo {
     public static void main(String args[]) {
-        MyClass t1 = new MyClass();
-        MyClass t2 = new MyClass();
+        MyClass t1 = new MyClass(10);
+        MyClass t2 = new MyClass(20);
         System.out.println(t1.x + " " + t2.x);
     }
 }
@@ -16,7 +16,7 @@ public class ConsDemo {
 class MyClass {
     int x;
 
-    MyClass() {
-        x = 10;
+    MyClass(int i) {
+        x = i;
     }
 }
