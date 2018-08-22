474d21237ffbcc613a16bfbcd3b790713301ac7b
Zhan Lingling <zhanlingling_00@163.com>
2018-08-22 15:00:52 +0800

    Scanner nextLine function.

--- src/ScannerDemo.java
+++ src/ScannerDemo.java
@@ -12,11 +12,17 @@ public class ScannerDemo {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("next方式接收：");
-
         if (scan.hasNext()) {
             String str1 = scan.next();
             System.out.println("输入的数据为：" + str1);
         }
+
+        System.out.println("nextLine方式接收：");
+        if (scan.hasNextLine()) {
+            String str2 = scan.nextLine();
+            System.out.println("输入的数据为：" + str2);
+        }
+
         scan.close();
     }
 }
