ca485537b0b7576ec76e38c37d30cb59f02a88a2
Zhan Lingling <zhanlingling_00@163.com>
2018-08-22 17:28:17 +0800

    Input multi numbers.

---
+++ src/ScannerDemo2.java
@@ -0,0 +1,27 @@
+/*************************************************************************
+	File Name: ScannerDemo2.java
+	Author: Zhan Lingling
+	Mail: zhan_lingling@sina.cn
+	Created Time: Wed Aug 22 16:45:41 2018
+ ************************************************************************/
+
+import java.util.Scanner;
+
+public class ScannerDemo2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double sum = 0;
+        int m = 0;
+
+        while (scan.hasNextDouble()) {
+            double x = scan.nextDouble();
+            m = m + 1;
+            sum = sum + x;
+        }
+
+        System.out.println(m + "个数的和为" + sum);
+        System.out.println(m + "个数的平均值为" + (sum / m));
+        scan.close();
+    }
+}
