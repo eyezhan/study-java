cf2816461d18cd5df77eb56c40c2c45858d9879e
Zhan Lingling <zhanlingling_00@163.com>
2018-08-22 14:58:07 +0800

    Scanner next function.

---
+++ src/ScannerDemo.java
@@ -0,0 +1,22 @@
+/*************************************************************************
+	File Name: ScannerDemo.java
+	Author: Zhan Lingling
+	Mail: zhan_lingling@sina.cn
+	Created Time: Wed Aug 22 14:50:23 2018
+ ************************************************************************/
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("next方式接收：");
+
+        if (scan.hasNext()) {
+            String str1 = scan.next();
+            System.out.println("输入的数据为：" + str1);
+        }
+        scan.close();
+    }
+}
